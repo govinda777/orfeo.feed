1d61682aff38d4ec5717d1882b6b9397a8fa7c04
Luã Govinda <govinda777@gmail.com>
2022-07-27 15:08:39 -0300

    implementacao de mock

--- dashboards/dashboard/src/app/modules/application/dashboard/00-main/items.ts
+++ dashboards/dashboard/src/app/modules/application/dashboard/00-main/items.ts
@@ -1,4 +1,7 @@
 export const ITEMS: any[] =
   [
-    { id: 1, name: 'Template', link: 'template', icon: 'far fa-address-card' }
+    
+     { id: 1, name: 'Card', link: 'card', icon: 'far fa-address-card' }
+    ,{ id: 2, name: 'Detail', link: 'detail', icon: 'far fa-address-card' }
+    ,{ id: 3, name: 'Template', link: 'template', icon: 'far fa-address-card' }
   ];
