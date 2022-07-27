daec248715231f5d401808f074f5192b5b94bd3a
Luã Govinda <govinda777@gmail.com>
2022-07-27 02:30:16 -0300

    update

---
+++ dashboards/dashboard/poc/angular-http-client/src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  application:
+  {
+    name: 'angular-ssr',
+    version: 'Angular 14.0.4',
+    bootstrap: 'Bootstrap 5.1.3',
+    fontawesome: 'Font Awesome 6.1.1',
+  }
+};
